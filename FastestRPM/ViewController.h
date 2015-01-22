209837ae525e50e99f662495121360a75da6a37b
manuntag <manuntag@ualberta.ca>
2015-01-22 11:01:32 -0800

    Initial Commit

---
+++ FastestRPM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FastestRPM
+//
+//  Created by David Manuntag on 2015-01-22.
+//  Copyright (c) 2015 David Manuntag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FastestRPM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FastestRPM
+//
+//  Created by David Manuntag on 2015-01-22.
+//  Copyright (c) 2015 David Manuntag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
